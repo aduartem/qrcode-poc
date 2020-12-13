f390efed908660d241a3471d5d7d7d05dacaf72c
Andrés Duarte M <andres.duartemarquez@cencosud.cl>
2020-12-13 13:51:32 -0300

    Botón para descargar código QR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,23 @@ export class AppComponent {
   public elementType = NgxQrcodeElementTypes.URL;
   public errorCorrectionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
   public value = 'http://www.youtube.com';
+
+  public downloadQRCode() {
+    const b64Img = document.getElementsByClassName('qrCode')[0].children[0]['src'];
+    fetch(b64Img)
+       .then(data => data.blob())
+       .then((blob) => {
+          const filename = 'image_qrcode';
+          // IE
+          if (window.navigator && window.navigator.msSaveOrOpenBlob){
+             window.navigator.msSaveOrOpenBlob(blob, filename);
+          } else { // Chrome
+             const url = window.URL.createObjectURL(blob);
+             const link = document.createElement('a');
+             link.href = url;
+             link.download = filename;
+             link.click();
+          }
+       })
+ }
 }
