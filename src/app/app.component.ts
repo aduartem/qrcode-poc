789f263c6e3db1d915216e3f86ae2eb5ffcd176c
Andrés Duarte M <andres.duartemarquez@cencosud.cl>
2020-12-13 13:39:00 -0300

    Generador de código QR en tiempo real

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'qrcode-poc';
-  elementType = 'url';
-  value = 'http://www.youtube.com';
+  public elementType = NgxQrcodeElementTypes.URL;
+  public errorCorrectionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
+  public value = 'http://www.youtube.com';
 }
